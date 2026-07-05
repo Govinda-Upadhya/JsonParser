71f0c72306716020668cef58112e1132f846c12b
Govinda <upadhyagovinda52@gmail.com>
2026-07-05 12:54:09 +0600

    created the parser that returns a abstract syntax tree of json

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,13 @@ impl Lexer {
     }
     fn read_stringtoken(&mut self)->String{
         let mut stringval=String::new();
-        while let Some(current)=self.peek(){
-            if current.is_alphabetic() && current !='"'{
-                stringval.push(current);
-                self.advance();
-            }else{
+        while let Some(current) = self.peek() {
+            if current == '"' {
                 break;
             }
+
+            stringval.push(current);
+            self.advance();
         }
         self.advance();
         stringval
@@ -146,9 +146,167 @@ impl Lexer {
         tokens
     }
 }
+#[derive(Debug)]
+pub enum JsonValue {
+    Object(Vec<(String, JsonValue)>),
+
+    Array(Vec<JsonValue>),
+
+    String(String),
+
+    Number(i64),
+
+    Boolean(bool),
+
+    Null,
+}
+impl JsonValue {
+    fn get(&self, key: &str) -> Option<&JsonValue> {
+        match self {
+            JsonValue::Object(pairs) => {
+                for (k, v) in pairs {
+                    if k == key {
+                        return Some(v);
+                    }
+                }
+                None
+            }
+
+            _ => None,
+        }
+    }
+}
+pub struct Parser{
+    input:Vec<Token>,
+    position:usize
+}
+impl Parser{
+    fn new(input:Vec<Token>)->Self{
+        Self { input, position: 0 }
+    }
+    fn peek(&self)->&Token{
+        if self.position < self.input.len() {
+            &self.input[self.position]
+        } else {
+            panic!("Unexpected end of token stream");
+        }
+    }
+    fn advance(&mut self){
+        self.position+=1;
+    }
+    fn expect(&mut self, description: &str) -> &Token {
+        if self.position < self.input.len() {
+            let token = &self.input[self.position];
+            self.position += 1;
+            token
+        } else {
+            panic!("Expected {} but reached end of input", description);
+        }
+    }
+    fn parse_value(&mut self)->JsonValue{
+        match self.peek(){
+            Token::OpenBraces    => self.parse_object(),
+            Token::ListStart  => self.parse_array(),
+            Token::Boolean(true)         => { self.advance(); JsonValue::Boolean(true)  }
+            Token::Boolean(false)        => { self.advance(); JsonValue::Boolean(false) }
+            Token::Null         => { self.advance(); JsonValue::Null        }
+            Token::NumberToken(n) => {
+                let value = *n;
+                self.advance();
+                JsonValue::Number(value)
+            }
+            Token::StringToken(s) => {
+                let value = s.clone();
+                self.advance();
+                JsonValue::String(value)
+            }
+            Token::CloseBraces   => panic!("Unexpected '}}'"),
+            Token::ListEnd => panic!("Unexpected ']'"),
+            Token::Colon        => panic!("Unexpected ':'"),
+            Token::Comma        => panic!("Unexpected ','"),
+        }
+    }
+    fn parse_object(&mut self)->JsonValue{
+        self.advance();
+        let mut pairs: Vec<(String, JsonValue)> = Vec::new();
+        if *self.peek() == Token::CloseBraces {
+            self.advance();
+            return JsonValue::Object(pairs);
+        }
+        loop {
+            let key = match self.expect("object key") {
+                Token::StringToken(s) => s.clone(),
+                _other => panic!("Expected string key, got something else"),
+            };
+
+            
+            match self.expect("colon") {
+                Token::Colon => {}
+                _ => panic!("Expected ':' after object key"),
+            }
+
+            
+            let value = self.parse_value();
+
+            pairs.push((key, value));
+            match self.peek() {
+                Token::Comma => {
+                    self.advance(); 
+                }
+                Token::CloseBraces => {
+                    self.advance(); 
+                    break;
+                }
+                _ => panic!("Expected ',' or '}}' in object"),
+            }
+        }
+        return JsonValue::Object(pairs);
+    }
+    fn parse_array(&mut self) -> JsonValue {
+        self.advance(); 
+
+        let mut elements: Vec<JsonValue> = Vec::new();
+
+       
+        if let Token::ListEnd = self.peek() {
+            self.advance();
+            return JsonValue::Array(elements);
+        }
+
+        loop {
+            let element = self.parse_value(); // recursive call
+            elements.push(element);
+
+            match self.peek() {
+                Token::Comma => {
+                    self.advance();
+                }
+                Token::ListEnd => {
+                    self.advance(); 
+                    break;
+                }
+                _ => panic!("Expected ',' or ']' in array"),
+            }
+        }
+
+        JsonValue::Array(elements)
+    }
+    fn parse(tokens: Vec<Token>) -> JsonValue {
+        let mut parser = Parser::new(tokens);
+        let value = parser.parse_value();
+        value
+    }
+}
 fn main(){
     let raw_string=r#"{"name": "alice", "age": 30, "scores": [95, 87, 100], "active": true}"#;
     let mut lexer= Lexer::new(raw_string);
     let tokens= lexer.tokenize();
     println!("{:#?}",tokens);
+    let json= Parser::parse(tokens);
+    if let Some(age) = json.get("age") {
+        println!("{:?}", age);
+    }
+   
+   
+    
 }
